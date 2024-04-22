ed9050c854db19e63132870e307a203c12ca3903
zarqman <tm@iprog.com>
2024-04-21 18:12:42 -0600

    Confirm that resource usage is still 0 before retiring. (#17)

--- lib/async/pool/controller.rb
+++ lib/async/pool/controller.rb
@@ -169,10 +169,12 @@ module Async
 				
 				# It's okay for this to context switch:
 				unused.each do |resource|
-					if block_given?
-						yield resource
-					else
-						retire(resource)
+					if usage = @resources[resource] and usage.zero?
+						if block_given?
+							yield resource
+						else
+							retire(resource)
+						end
 					end
 					
 					break if @resources.size <= retain
